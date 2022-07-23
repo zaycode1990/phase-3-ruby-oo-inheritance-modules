5a52ef4142770c260d097fddf3df40792fa59712
Isaiah <ijandicos90@gmail.com>
2022-07-22 21:16:42 -0400

    commit tree e33a0c80d6174c6308d0c1fe6be7f30231854749

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,25 @@
+# with non-namespaced modules
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Kid
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+# with namespaced modules
+# require_relative './fancy_dance'
+# class Kid
+#   include FancyDance::InstanceMethods
+#   extend FancyDance::ClassMethods
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+# end
